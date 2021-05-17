aa6d06ce001b9777f958db8e11c84101afd309ba
Fernando Batista <foliveira@expertvoice.com.br>
2021-05-17 15:06:41 -0300

    Starting job

---
+++ src/components/Dashboard/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="counter">
+    Counter: {{ counter }}
+  </div>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 });
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
